6e486e43d5930b2c9e190c3f596f7f29e23076f2
Romain Ruaud <romain.ruaud@smile.fr>
2016-07-19 16:36:50 +0200

    Adding a column to display eventual overlap of offers.

---
+++ Ui/Component/Offer/Listing/AddHasOverlapFilterToCollection.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * DISCLAIMER
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile\RetailerOffer
+ * @author    Romain Ruaud <romain.ruaud@smile.fr>
+ * @copyright 2016 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+namespace Smile\RetailerOffer\Ui\Component\Offer\Listing;
+
+use Magento\Ui\DataProvider\AddFilterToCollectionInterface;
+use Magento\Framework\Data\Collection;
+
+/**
+ * Filter strategy for "has_overlap" field
+ *
+ * @category Smile
+ * @package  Smile\RetailerOffer
+ * @author   Romain Ruaud <romain.ruaud@smile.fr>
+ */
+class AddHasOverlapFilterToCollection implements AddFilterToCollectionInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function addFilter(Collection $collection, $field, $condition = null)
+    {
+        if (isset($condition["eq"])) {
+            $collection->addHasOverlapFilter($field, $condition["eq"]);
+        }
+    }
+}
