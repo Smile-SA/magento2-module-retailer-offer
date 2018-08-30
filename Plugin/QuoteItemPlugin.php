8419acb062b1335db1e900d719b4779aca7dabf2
Maxime LECLERCQ <maxime.leclercq@smile.fr>
2018-08-30 16:42:03 +0200

    Fix #18 - Fix prices compare when use taxes on product

--- Plugin/QuoteItemPlugin.php
+++ Plugin/QuoteItemPlugin.php
@@ -42,23 +42,31 @@ class QuoteItemPlugin
     private $eventManager;
 
     /**
+     * @var \Magento\Tax\Model\Config
+     */
+    private $taxConfig;
+
+    /**
      * ProductPlugin constructor.
      *
      * @param \Smile\RetailerOffer\Helper\Offer             $offerHelper    The offer Helper
      * @param \Smile\StoreLocator\CustomerData\CurrentStore $currentStore   The Current Store object
      * @param \Smile\RetailerOffer\Helper\Settings          $settingsHelper Settings Helper
      * @param \Magento\Framework\Event\ManagerInterface     $eventManager   The Event Manager
+     * @param \Magento\Tax\Model\Config                     $taxConfig      Tax config.
      */
     public function __construct(
         \Smile\RetailerOffer\Helper\Offer $offerHelper,
         \Smile\StoreLocator\CustomerData\CurrentStore $currentStore,
         \Smile\RetailerOffer\Helper\Settings $settingsHelper,
-        \Magento\Framework\Event\ManagerInterface $eventManager
+        \Magento\Framework\Event\ManagerInterface $eventManager,
+        \Magento\Tax\Model\Config $taxConfig
     ) {
         $this->offerHelper    = $offerHelper;
         $this->currentStore   = $currentStore;
         $this->settingsHelper = $settingsHelper;
         $this->eventManager   = $eventManager;
+        $this->taxConfig      = $taxConfig;
     }
 
     /**
@@ -88,8 +96,9 @@ class QuoteItemPlugin
             if ($currentOffer) {
                 $offerPrice = $currentOffer->getSpecialPrice() ? $currentOffer->getSpecialPrice() : $currentOffer->getPrice();
             }
+            $resultItemPrice = $this->getQuoteItemPrice($resultItem);
 
-            if ($offerPrice && $resultItem->getPrice() && ((float) $resultItem->getPrice() !== (float) $offerPrice)) {
+            if ($offerPrice && $resultItemPrice && ((float) $resultItemPrice !== (float) $offerPrice)) {
                 $resultItem->setPrice($offerPrice);
                 if ($resultItem->getQuote()) {
                     $resultItem->getQuote()->setTotalsCollectedFlag(false)->collectTotals()->save();
@@ -103,5 +112,17 @@ class QuoteItemPlugin
         }
 
         return $resultItem;
+    }
+
+    /**
+     * Return quote item price (include or exclude tax).
+     *
+     * @param \Magento\Quote\Model\Quote\Item $item
+     *
+     * @return float|null
+     */
+    private function getQuoteItemPrice(\Magento\Quote\Model\Quote\Item $item)
+    {
+        return !$this->taxConfig->priceIncludesTax() ? $item->getPrice() : $item->getPriceInclTax();
     }
 }
